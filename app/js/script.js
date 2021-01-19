6a9815357e9d94292ed53ce73f31a36c60abe798
Optimixto <alegarmun@gmail.com>
2021-01-19 17:50:35 +0100

    Finished Header
    Included header's menu animation and overlay.

--- app/js/script.js
+++ app/js/script.js
@@ -2,19 +2,30 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+const body = document.querySelector('body');
 
 btnHamburger.addEventListener('click', function(){
-    if(header.classList.contains('open')){
+    if(header.classList.contains('open')){ //closes
         header.classList.remove('open');
-        
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
+
+        body.classList.remove('locked-scroll');
+
+        fadeElems.forEach(function(elem){
+            elem.classList.remove('fade-in');
+            elem.classList.add('fade-out');
+        });
     }
-    else
+    else //opens
     {
         header.classList.add('open');
+        
+        body.classList.add('locked-scroll');
 
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in');
+        fadeElems.forEach(function(elem){
+            elem.classList.remove('fade-out');
+            elem.classList.add('fade-in');
+        });
+        
     }
 });
